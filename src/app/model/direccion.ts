a966829377ce36d426d4400ec430d4f7599bfeff
vricheb973 <vricheb973@g.educaand.es>
2025-12-02 13:14:48 +0100

    inicio del boletin 2 poo_angular

---
+++ src/app/model/direccion.ts
@@ -0,0 +1,12 @@
+export class Direccion {
+    
+    // Atributos de la clase
+    private _idDireccion: number;
+    private _codPostal: string;
+    private _ciudad: string;
+    private _municipio: string;
+    private _provincia: string;
+
+    // Constructor/es
+
+}
