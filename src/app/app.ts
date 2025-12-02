e91e07033ebe4a5d9afd9b6b6f7ff5034f5ba0c4
vricheb973 <vricheb973@g.educaand.es>
2025-12-02 13:42:53 +0100

    getters y setters + encapsulacion

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Direccion } from './model/direccion';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App{
   protected readonly title = signal('boletin2');
+  public direccion: Direccion = new Direccion(1,"41510", 'Sevilla','Mairena del Alcor', 'Sevilla');
+  
 }
--- src/app/model/direccion.ts
+++ src/app/model/direccion.ts
@@ -1,12 +1,26 @@
 export class Direccion {
     
-    // Atributos de la clase
-    private _idDireccion: number;
-    private _codPostal: string;
-    private _ciudad: string;
-    private _municipio: string;
-    private _provincia: string;
+    // Constructor de tipo firma
+    constructor(
+        private _idDireccion: number, 
+        private _codPostal: string, 
+        private _ciudad: string, 
+        private _municipio: string, 
+        private _provincia: string
+    ){};
 
-    // Constructor/es
+    //Getters y setters
+    get idDireccion(): number { return this._idDireccion; }
+    get codPostal(): string { return this._codPostal; }
+    get ciudad(): string { return this._ciudad; }
+    get municipio(): string { return this._municipio; }
+    get provincia(): string { return this._provincia; }
+
+    set idDireccion(idDireccion: number) { this._idDireccion = idDireccion; }
+    set codPostal(codPostal: string) { this._codPostal = codPostal; }
+    set ciudad(ciudad: string) { this._ciudad = ciudad; }
+    set municipio(municipio: string) { this._municipio = municipio; }
+    set provincia(provincia: string) { this._provincia = provincia; }
+
 
 }
