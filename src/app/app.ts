5ac3065336916c037d0a5c0bf04bc14be48f41b4
vricheb973 <vricheb973@g.educaand.es>
2025-12-02 13:49:58 +0100

    prueba alumno

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Direccion } from './model/direccion';
 import { FormsModule } from '@angular/forms';
+import { Alumno } from './model/alumno';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,7 @@ import { FormsModule } from '@angular/forms';
 })
 export class App{
   protected readonly title = signal('boletin2');
-  public direccion: Direccion = new Direccion(1,"41510", 'Sevilla','Mairena del Alcor', 'Sevilla');
+  public direccion: Direccion = new Direccion(1,"41510", 'Sevilla','Mairena del Alcor', 'Sevilla'); 
+  public alumno = new Alumno("dni","nombre","apellidos",new Date(), this.direccion);
   
 }
