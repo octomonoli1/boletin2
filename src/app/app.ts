e3bd2c42bde5b05325ecdeeae5a01c22db1d62c4
vricheb973 <vricheb973@g.educaand.es>
2025-12-09 12:17:07 +0100

    arreglo que no recorria alumnos

--- src/app/app.ts
+++ src/app/app.ts
@@ -23,13 +23,16 @@ export class App{
   public alumno2 = new Alumno("00000000T","Sara","Dominguez Sanchez",new Date(), this.direccion2);
   public profesor1 = new Profesor("12345678J", "Vladi", "Rico Hebles", "600123456", 3500);
   public profesor2 = new Profesor("87654321P", "Leo", "Bernal Bueno", "777666222", 1500);
+  public profesor3 = new Profesor("93281212J", "Octavio", "Montero Oliva", "777666222", 1500);
   public asignatura1 = new Asignatura("Despliegue de aplicaiones web", "Gestion y configuracion de servidores", 70, this.profesor2);
   public asignatura2 = new Asignatura("Proyecto Integrado", "Proyecto real de desarrollo software", 75, this.profesor1);
+  public asignatura3 = new Asignatura("Desarrollo web cliente", "Programación frontend", 192, this.profesor3);
   public matricula1 = new Matricula(this.alumno1, this.asignatura1, 10,8,8);
   public matricula2 = new Matricula(this.alumno1, this.asignatura2, 7,6,4);
   public matricula3 = new Matricula(this.alumno2, this.asignatura1, 7,8,1);
   public matricula4 = new Matricula(this.alumno2, this.asignatura2, 5, 4, 6);
-  public matriculas: Matricula[] = [this.matricula1, this.matricula2, this.matricula3, this.matricula4];
+  public matricula5 = new Matricula(this.alumno2, this.asignatura3, 0, 8, 8);
+  public matriculas: Matricula[] = [this.matricula1, this.matricula2, this.matricula3, this.matricula4,this.matricula5];
 
   public getAlumnos(): Alumno[]{
     let alumnosDistintos: Alumno[] = [];
